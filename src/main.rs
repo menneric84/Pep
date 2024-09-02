8f64c22b8f7dc7f438cdf4ae104a0621431db617
Rick <menneric84@gmail.com>
2024-09-02 09:52:48 -0500

    shell of editor

--- src/main.rs
+++ src/main.rs
@@ -1,108 +1,151 @@
-use std::io::{stdout, Write};
-
 use crossterm::{
     cursor,
-    event::{self, read},
-    style, terminal, ExecutableCommand, QueueableCommand,
+    event::{read, Event, KeyCode, KeyEvent},
+    terminal::{self, ClearType, EnterAlternateScreen, LeaveAlternateScreen},
+    ExecutableCommand, QueueableCommand,
 };
 
-enum Action {
-    Quit,
-    MoveUp,
-    MoveDown,
-    MoveLeft,
-    MoveRight,
-
-    EnterMode(Mode),
-}
+use std::io::{stdout, Stdout, Write};
 
 enum Mode {
     Normal,
     Insert,
 }
 
-fn handle_event(
-    mode: &Mode,
-    stdout: &mut std::io::Stdout,
-    ev: event::Event,
-) -> anyhow::Result<Option<Action>> {
-    match mode {
-        Mode::Normal => handle_normal_event(ev),
-        Mode::Insert => handle_insert_event(stdout, ev),
-    }
+enum Command {
+    InsertMode = Char('i'),
+    Normal,
 }
 
-fn handle_normal_event(ev: event::Event) -> anyhow::Result<Option<Action>> {
-    match ev {
-        event::Event::Key(event) => match event.code {
-            event::KeyCode::Char('q') => Ok(Some(Action::Quit)),
-            event::KeyCode::Up | event::KeyCode::Char('k') => Ok(Some(Action::MoveUp)),
-            event::KeyCode::Down | event::KeyCode::Char('j') => Ok(Some(Action::MoveDown)),
-            event::KeyCode::Left | event::KeyCode::Char('h') => Ok(Some(Action::MoveLeft)),
-            event::KeyCode::Right | event::KeyCode::Char('l') => Ok(Some(Action::MoveRight)),
-            event::KeyCode::Char('i') => Ok(Some(Action::EnterMode(Mode::Insert))),
-            _ => Ok(None),
-        },
-        _ => Ok(None),
-    }
+struct Cursor {
+    x: u16,
+    y: u16,
 }
 
-fn handle_insert_event(
-    stdout: &mut std::io::Stdout,
-    ev: event::Event,
-) -> anyhow::Result<Option<Action>> {
-    match ev {
-        event::Event::Key(event) => match event.code {
-            event::KeyCode::Esc => Ok(Some(Action::EnterMode(Mode::Normal))),
-            event::KeyCode::Char(c) => {
-                stdout.queue(style::Print(c))?;
-                return Ok(Some(Action::MoveRight));
+struct Editor {
+    out: Stdout,
+    cursor: Cursor,
+    mode: Mode,
+}
+
+trait NewTrait {
+    fn handle_insert_key_event_with_modifier(&mut self, event: KeyEvent);
+    fn handle_normal_key_event_with_modifier(&mut self, event: KeyEvent);
+    fn handle_insert_key_event(&mut self, event: KeyEvent);
+    fn handle_normal_key_event(&mut self, event: KeyEvent);
+    fn handle_key_event(&mut self, event: KeyEvent);
+    fn leave_alt_screen(&mut self);
+    fn flush(&mut self);
+    fn move_cursor(&mut self, x: u16, y: u16);
+    fn enter_insert_mode(&mut self);
+    fn clear(&mut self);
+    fn enter_alt_screen(&mut self);
+}
+
+impl NewTrait for Editor {
+    fn move_cursor(&mut self, x: u16, y: u16) {
+        self.cursor.x = x;
+        self.cursor.y = y;
+        self.out.queue(cursor::MoveTo(x, y)).unwrap();
+    }
+
+    fn enter_insert_mode(&mut self) {
+        self.mode = Mode::Insert;
+    }
+
+    fn clear(&mut self) {
+        self.out.execute(terminal::Clear(ClearType::All)).unwrap();
+    }
+
+    fn enter_alt_screen(&mut self) {
+        self.out.execute(EnterAlternateScreen).unwrap();
+    }
+
+    fn leave_alt_screen(&mut self) {
+        self.out.execute(LeaveAlternateScreen).unwrap();
+    }
+
+    fn flush(&mut self) {
+        self.out.flush().unwrap();
+    }
+
+    fn handle_normal_key_event(&mut self, event: KeyEvent) {
+        match event.code {
+            KeyCode::Char('i') => {
+                self.enter_insert_mode();
             }
-            _ => Ok(None),
-        },
-        _ => Ok(None),
+            _ => {}
+        }
+    }
+
+    fn handle_normal_key_event_with_modifier(&mut self, event: KeyEvent) {
+        match event.code {
+            _ => {}
+        }
+    }
+
+    fn handle_insert_key_event(&mut self, event: KeyEvent) {
+        match event.code {
+            _ => {}
+        }
+    }
+
+    fn handle_insert_key_event_with_modifier(&mut self, event: KeyEvent) {
+        match event.code {
+            _ => {}
+        }
+    }
+
+    fn handle_key_event(&mut self, event: KeyEvent) {
+        match self.mode {
+            Mode::Normal => {
+                if crossterm::event::KeyModifiers::is_empty(&event.modifiers) {
+                    self.handle_normal_key_event(event);
+                } else {
+                    self.handle_normal_key_event_with_modifier(event);
+                }
+            }
+            Mode::Insert => {
+                if event.modifiers.is_empty() {
+                    self.handle_insert_key_event(event);
+                } else {
+                    self.handle_insert_key_event_with_modifier(event);
+                }
+            
+            }
+        }
     }
 }
 
 fn main() -> anyhow::Result<()> {
-    let mut stdout = stdout();
-    let mut mode = Mode::Normal;
-    let mut cx = 0;
-    let mut cy = 0;
-
+    let mut editor = Editor {
+        out: stdout(),
+        cursor: Cursor { x: 0, y: 0 },
+        mode: Mode::Normal,
+    };
+    // Disable default terminal before like input handling and Ctrl modifiers
     terminal::enable_raw_mode()?;
-    stdout.execute(terminal::EnterAlternateScreen)?;
 
-    stdout.execute(terminal::Clear(terminal::ClearType::All))?;
+    // Clear the terminal
+    editor.clear();
+
+    // Enter alt screen
+    editor.enter_alt_screen();
+
+    // Position cursor at origin
+    editor.move_cursor(0, 0);
 
     loop {
-        stdout.queue(cursor::MoveTo(cx, cy))?;
-        stdout.flush()?;
-
-        if let Some(action) = handle_event(&mode, &mut stdout, read()?)? {
-            match action {
-                Action::Quit => break,
-                Action::MoveUp => {
-                    cy = cy.saturating_sub(1);
-                }
-                Action::MoveDown => {
-                    cy += 1u16;
-                }
-                Action::MoveLeft => {
-                    cx = cx.saturating_sub(1);
-                }
-                Action::MoveRight => {
-                    cx += 1u16;
-                }
-                Action::EnterMode(new_mode) => {
-                    mode = new_mode;
-                }
-            }
+        // Run any queued commands
+        editor.flush();
+        match read()? {
+            Event::Key(event) => println!("{:?}", event),
+            _ => {}
         }
+
+        break;
     }
+    editor.leave_alt_screen();
 
-    stdout.execute(terminal::LeaveAlternateScreen)?;
-    terminal::disable_raw_mode()?;
-
-    Ok(())
+    return Ok(());
 }
